4bfee7f311a49ce5468524e29a64030bfdc7ecc9
Angelina-Pinchuk <angelina_pinchuk2206@mail.ru>
2025-11-23 20:22:19 +0300

    fix

--- my-script.js
+++ my-script.js
@@ -1,12 +1,14 @@
-// Создаём кнопку
-const btn = document.createElement('button');
-btn.textContent = 'Run script';
-btn.className = 'my-run-btn'; // назначаем определённый класс
+window.addEventListener('DOMContentLoaded', () => {
+    // Создаём кнопку
+    const btn = document.createElement('button');
+    btn.textContent = 'Run script';
+    btn.className = 'my-run-btn'; // назначаем определённый класс
 
 // Добавляем кнопку в начало body
-document.body.insertBefore(btn, document.body.firstChild);
+    document.body.insertBefore(btn, document.body.firstChild);
 
 // Вешаем обработчик на кнопку
-btn.addEventListener('click', () => {
-    alert('script is running');
+    btn.addEventListener('click', () => {
+        alert('script is running');
+    });
 });
